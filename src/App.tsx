dbd3eb2d45a44fb5175652df4b260e911e9ce113
Kseniia Stativka <kseniia.stativka@gmail.com>
2021-02-24 20:43:27 +0100

    feat: add hide completed todos option

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ type Todos = Infer<typeof TodosStruct>
 function App() {
   const [todoInput, setTodoInput] = useState("")
   const [todos, setTodos] = useState<Todos>([])
+  const [visibility, setVisibility] = useState<"all" | "incomplete">("all")
   const isFirstRender = useRef(true)
 
   useEffect(() => {
@@ -102,50 +103,55 @@ function App() {
           Sort todos
         </button>
 
-        <ol className="todo-list">
-          {todos.map((todo, todoIndex) => (
-            <li key={todo.id}>
-              <div className="todo-list-item">
-                {todo.isInEditMode ? (
-                  <input
-                    type="text"
-                    value={todo.name}
-                    onChange={(event) => {
-                      setTodos((prevToDos) => {
-                        return prevToDos.map((prevTodo) => {
-                          if (prevTodo.id === todo.id) {
-                            return {
-                              ...prevTodo,
-                              name: event.target.value,
-                            }
-                          }
-                          return prevTodo
-                        })
-                      })
-                    }}
-                  />
-                ) : (
-                  <div className="todo-list-item__text-container">
-                    <span
-                      className={`todo-list-item__text ${
-                        todo.isCompleted
-                          ? "todo-list-item__text--completed"
-                          : ""
-                      }`}
-                    >
-                      {todo.name}
-                    </span>
+        <button
+          onClick={() => {
+            setVisibility((prevVisibility) => {
+              switch (prevVisibility) {
+                case "all":
+                  return "incomplete"
 
+                case "incomplete":
+                  return "all"
+              }
+            })
+          }}
+        >
+          {(() => {
+            switch (visibility) {
+              case "all":
+                return "Hide completed"
+
+              case "incomplete":
+                return "Show all"
+            }
+          })()}
+        </button>
+
+        <ol className="todo-list">
+          {todos
+            .filter((todo) => {
+              switch (visibility) {
+                case "all":
+                  return true
+
+                case "incomplete":
+                  return !todo.isCompleted
+              }
+            })
+            .map((todo, todoIndex) => (
+              <li key={todo.id}>
+                <div className="todo-list-item">
+                  {todo.isInEditMode ? (
                     <input
-                      type="checkbox"
-                      checked={todo.isCompleted}
+                      type="text"
+                      value={todo.name}
                       onChange={(event) => {
-                        setTodos((prevState) => {
-                          return prevState.map((prevTodo) => {
+                        setTodos((prevToDos) => {
+                          return prevToDos.map((prevTodo) => {
                             if (prevTodo.id === todo.id) {
                               return {
                                 ...prevTodo,
-                                isCompleted: event.target.checked,
+                                name: event.target.value,
                               }
                             }
                             return prevTodo
@@ -153,96 +159,125 @@ function App() {
                         })
                       }}
                     />
-                  </div>
-                )}
-                <div className="todo-list-item__buttons">
-                  <button
-                    className="button"
-                    onClick={() => {
-                      setTodos((prevTodos) => {
-                        return prevTodos.map((prevTodo) => {
-                          if (prevTodo.id === todo.id) {
-                            return {
-                              ...prevTodo,
-                              isInEditMode: !prevTodo.isInEditMode,
-                            }
-                          }
+                  ) : (
+                    <div className="todo-list-item__text-container">
+                      <span
+                        className={`todo-list-item__text ${
+                          todo.isCompleted
+                            ? "todo-list-item__text--completed"
+                            : ""
+                        }`}
+                      >
+                        {todo.name}
+                      </span>
 
-                          return prevTodo
-                        })
-                      })
-                    }}
-                  >
-                    Edit
-                  </button>
-
-                  <button
-                    className="button"
-                    onClick={() => {
-                      setTodos((prevTodos) => {
-                        return prevTodos.filter((_, i) => {
-                          return i !== todoIndex
-                        })
-                      })
-                    }}
-                  >
-                    Delete
-                  </button>
-
-                  <button
-                    className="button"
-                    onClick={() => {
-                      setTodos((prevTodos) =>
-                        prevTodos
-                          .slice(0, todoIndex + 1)
-                          .concat({
-                            id: Date.now(),
-                            name: todo.name,
-                            isInEditMode: false,
-                            isCompleted: false,
+                      <input
+                        type="checkbox"
+                        checked={todo.isCompleted}
+                        onChange={(event) => {
+                          setTodos((prevState) => {
+                            return prevState.map((prevTodo) => {
+                              if (prevTodo.id === todo.id) {
+                                return {
+                                  ...prevTodo,
+                                  isCompleted: event.target.checked,
+                                }
+                              }
+                              return prevTodo
+                            })
                           })
-                          .concat(prevTodos.slice(todoIndex + 1)),
-                      )
-                    }}
-                  >
-                    Copy
-                  </button>
+                        }}
+                      />
+                    </div>
+                  )}
+                  <div className="todo-list-item__buttons">
+                    <button
+                      className="button"
+                      onClick={() => {
+                        setTodos((prevTodos) => {
+                          return prevTodos.map((prevTodo) => {
+                            if (prevTodo.id === todo.id) {
+                              return {
+                                ...prevTodo,
+                                isInEditMode: !prevTodo.isInEditMode,
+                              }
+                            }
 
-                  <button
-                    className="button"
-                    onClick={() => {
-                      setTodos((prevTodos) =>
-                        prevTodos
-                          .slice(0, todoIndex)
-                          .concat([prevTodos[todoIndex + 1], todo])
-                          .concat(prevTodos.slice(todoIndex + 2)),
-                      )
-                    }}
-                    aria-label="Move down"
-                    disabled={todos.length === todoIndex + 1}
-                  >
-                    ⬇
-                  </button>
+                            return prevTodo
+                          })
+                        })
+                      }}
+                    >
+                      Edit
+                    </button>
 
-                  <button
-                    className="button"
-                    onClick={() => {
-                      setTodos((prevTodos) =>
-                        prevTodos
-                          .slice(0, todoIndex - 1)
-                          .concat([todo, prevTodos[todoIndex - 1]])
-                          .concat(prevTodos.slice(todoIndex + 1)),
-                      )
-                    }}
-                    aria-label="Move up"
-                    disabled={0 === todoIndex}
-                  >
-                    ⬆
-                  </button>
+                    <button
+                      className="button"
+                      onClick={() => {
+                        setTodos((prevTodos) => {
+                          return prevTodos.filter((_, i) => {
+                            return i !== todoIndex
+                          })
+                        })
+                      }}
+                    >
+                      Delete
+                    </button>
+
+                    <button
+                      className="button"
+                      onClick={() => {
+                        setTodos((prevTodos) =>
+                          prevTodos
+                            .slice(0, todoIndex + 1)
+                            .concat({
+                              id: Date.now(),
+                              name: todo.name,
+                              isInEditMode: false,
+                              isCompleted: false,
+                            })
+                            .concat(prevTodos.slice(todoIndex + 1)),
+                        )
+                      }}
+                    >
+                      Copy
+                    </button>
+
+                    <button
+                      className="button"
+                      onClick={() => {
+                        setTodos((prevTodos) =>
+                          prevTodos
+                            .slice(0, todoIndex)
+                            .concat([prevTodos[todoIndex + 1], todo])
+                            .concat(prevTodos.slice(todoIndex + 2)),
+                        )
+                      }}
+                      aria-label="Move down"
+                      disabled={todos.length === todoIndex + 1}
+                    >
+                      ⬇
+                    </button>
+
+                    <button
+                      className="button"
+                      onClick={() => {
+                        setTodos((prevTodos) =>
+                          prevTodos
+                            .slice(0, todoIndex - 1)
+                            .concat([todo, prevTodos[todoIndex - 1]])
+                            .concat(prevTodos.slice(todoIndex + 1)),
+                        )
+                      }}
+                      aria-label="Move up"
+                      disabled={0 === todoIndex}
+                    >
+                      ⬆
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))}
         </ol>
       </main>
 
